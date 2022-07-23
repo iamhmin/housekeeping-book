4b4a4702894bf4d4213ae53467795cfe55e3ec8a
haminseong <haminseong@com2us.com>
2022-07-24 05:28:41 +0900

    integrate next.js into a nest.js application

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home: FC = () => {
     <div>
       <Title mark level={3}>
             가계부 'AKKEO'에 오신 것을 환영합니다.
+            서버통합완료
       </Title>
     </div>
   );
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Record } from './entities/record.entity';
 import { RecordsModule } from './records/records.module';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
 
 @Module({
   imports: [
@@ -17,6 +19,12 @@ import { RecordsModule } from './records/records.module';
       entities: [User, Record],
       synchronize: true,
     }),
+    RenderModule.forRootAsync(
+      Next({ dev: true }),
+      /* null means that nest-next 
+          should look for pages in root dir */
+      { viewsDir: null },
+    ),
     RecordsModule
   ],
   controllers: [AppController],
